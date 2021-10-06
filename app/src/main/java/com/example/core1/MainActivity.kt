cf450df807b1c5cbc13f49960e249440876264e2
kumar9900 <81544321+kumar9900@users.noreply.github.com>
2021-10-07 07:35:32 +1100

    Core requirements done has 3 buttons 2 layouts- one linear and one constraint(landscape version)

--- app/src/main/java/com/example/core1/MainActivity.kt
+++ app/src/main/java/com/example/core1/MainActivity.kt
@@ -14,19 +14,22 @@ class MainActivity : AppCompatActivity() {
         val increment = 1//increment and decrement operator
         var total = 0//create a variable for the total value
         val reset = findViewById<Button>(R.id.Reset)//reset button set to val reset
-        val score = findViewById<Button>(R.id.score)
-        val steal = findViewById<Button>(R.id.steal)
+        val score = findViewById<Button>(R.id.score)//score button set to val score
+        val steal = findViewById<Button>(R.id.steal)//steal button set to val steal
         reset.setOnClickListener {
-            if (total==0){
-                findViewById<TextView>(R.id.total).setTextColor(Color.BLACK) //if the total value is greater than 4 then set the text view id to blue colour
-            }
             findViewById<TextView>(R.id.total).text = count.toString()//reset text view string to 0
             total = 0
+            if (total == 0) {
+                findViewById<TextView>(R.id.total).setTextColor(Color.BLACK)
+            } //if the total value is equal to 0 then set the text view id to black colour
         }
         score.setOnClickListener {
             if (total < 0) {
                 total = 0
             }//clamp the value for the total so that it wont go into negatives
+            if (total > 14) {
+                total = 14
+            }//clamp value
             total += increment//increment the total by 1
             if (total > 4) {
                 findViewById<TextView>(R.id.total).setTextColor(Color.BLUE) //if the total value is greater than 4 then set the text view id to blue colour
@@ -40,12 +43,12 @@ class MainActivity : AppCompatActivity() {
         steal.setOnClickListener {
             total -= increment//total decrements by one for evey click
             if (total < 0) {
-                total = 0
-            }// clamp the value for total so that it will not be a negative
-            if (total<5){
-                findViewById<TextView>(R.id.total).setTextColor(Color.BLACK) //if the total value is greater than 4 then set the text view id to blue colour
+                total = 0// clamp the value for total so that it will not be a negative
             }
-            if (total<11){
+            if (total == 0|| total<5) {
+                findViewById<TextView>(R.id.total).setTextColor(Color.BLACK)//if the total value is equal to 0 or less than 4 then set the text view id to black colour
+            }
+            if (total < 11) {
                 findViewById<TextView>(R.id.total).setTextColor(Color.BLUE) //if the total value is greater than 4 then set the text view id to blue colour
             }
             findViewById<TextView>(R.id.total).text =
