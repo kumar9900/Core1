4aa1fabc189e053dda486dce7e30cc526d8b64f1
kumar9900 <mkumar1996@hotmail.com>
2021-10-09 22:24:00 +1100

    Core requirements done has 3 buttons 2 layouts- one linear and one constraint(landscape version) App can rotate without losing total score. the score changes colour based on value winning sound plays when player has reached 15

--- app/src/main/java/com/example/core1/MainActivity.kt
+++ app/src/main/java/com/example/core1/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.core1
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
     var total: Int = 0
@@ -13,58 +14,70 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("LifeCycle", "onCreate")
-        val count = 0//initial counter value after reset
+        val textTotal = findViewById<TextView>(R.id.total)
         val increment = 1//increment and decrement operator
         val reset = findViewById<Button>(R.id.Reset)//reset button set to val reset
         val score = findViewById<Button>(R.id.score)//score button set to val score
         val steal = findViewById<Button>(R.id.steal)//steal button set to val steal
         savedInstanceState?.let {
             total = it.getInt("Total")
-            findViewById<TextView>(R.id.total).text = total.toString()
+            textTotal.text = total.toString()
         }
         reset.setOnClickListener {
-            findViewById<TextView>(R.id.total).text = count.toString()//reset text view string to 0
             total = 0
-            if (total == 0) {
-                findViewById<TextView>(R.id.total).setTextColor(Color.BLACK)
-            } //if the total value is equal to 0 then set the text view id to black colour
-        }
+            textTotal.setTextColor(updateColor(total)) // get return value from
+            textTotal.text = total.toString()//send total variable to text view
+            }
+
         score.setOnClickListener {
-            if (total < 0) {
-                total = 0
-            }//clamp the value for the total so that it wont go into negatives
-            if (total > 14) {
-                total = 14
-            }//clamp value
+
             total += increment//increment the total by 1
-            if (total > 4) {
-                findViewById<TextView>(R.id.total).setTextColor(Color.BLUE) //if the total value is greater than 4 then set the text view id to blue colour
+            total = clamp(total, 0, 15)
+            textTotal.setTextColor(updateColor(total))
+            textTotal.text = total.toString()//send total variable to text view
+            if (total==15)
+            {
+                winningSound()
             }
-            if (total > 9) {
-                findViewById<TextView>(R.id.total).setTextColor(Color.GREEN) //if the total value is greater than 9 then set the text view id to black colour
-            }
-            findViewById<TextView>(R.id.total).text =
-                total.toString()//send total variable to text view
         }
         steal.setOnClickListener {
             total -= increment//total decrements by one for evey click
-            if (total < 0) {
-                total = 0// clamp the value for total so that it will not be a negative
-            }
-            if (total < 11) {
-                findViewById<TextView>(R.id.total).setTextColor(Color.BLUE) //if the total value is greater than 4 then set the text view id to blue colour
-            }
-            if (total == 0 || total < 5) {
-                findViewById<TextView>(R.id.total).setTextColor(Color.BLACK)//if the total value is equal to 0 or less than 4 then set the text view id to black colour
-            }
-            findViewById<TextView>(R.id.total).text =
-                total.toString()//send total variable to the text view
+            total = clamp(total, 0, 15)//use clamp value function to set min and max values
+            textTotal.setTextColor(updateColor(total))//use update color function to change color based on value
+            textTotal.text = total.toString()//send total variable to the text view
         }
     }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt("Total", total)
-        Log.i("LifeCycle", "SavedInstanceState $total")
+        outState.putInt("Total", total)//save the total variable for next instance of the activity
+        Log.i("LifeCycle", "SavedInstanceState $total")//show message in log cat with lifecycle tag and with msg savedInstanceState and value of total
+    }
+//using the function to keep the max and min values as specified above
+    private fun clamp(value: Int, min: Int, max: Int): Int {
+        var newValue:Int = value
+        if (newValue < min)
+        {
+            newValue = min
+        }//clamp the value for the total so that it wont go into negatives
+        if (newValue > max)
+        {
+            newValue = max
+        }//clamp value
+        return newValue
+    }
+    private fun updateColor(value: Int): Int {
+        var newColor: Int = Color.BLACK // default value
+
+        if (value >= 10)
+            newColor = Color.GREEN//if value is greater than or equal to 10 the color is changed to green
+        else if (value >= 5)//else if the value is greater than or equal to 5 the color is changed to blue
+            newColor = Color.BLUE
+        return newColor//return the value of new color based on the if statements
+    }
+    private fun winningSound(){
+        val mediaPlayer = MediaPlayer.create(this, R.raw.dinner_bell_triangle)
+        mediaPlayer.start() // no need to call prepare(); create() does that for you
+        return
+
     }
 }
